34092819c8d8617889eda31e168124f66653787a
Aminul Islam <aminultension@gmail.com>
2023-09-16 10:16:52 +0530

    Fibonacci of a number using rec,memo & Tabulation

---
+++ DP_Striver/basicConcepts/fibonacii_all.java
@@ -0,0 +1,45 @@
+
+package basicConcepts;
+// fibonacci series using recursion, memoization and tabulation
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class fibonacii_all {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        int n = sc.nextInt();
+        sc.close();
+        System.out.println("Recursin : " + fibRecursive(n));
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        System.out.println("Memoization : " + fibMemoized(n, dp));
+        System.out.println("Tabulation : " + fibTabulation(n));
+    }
+
+    private static int fibRecursive(int n) {
+        if (n <= 1) return n;
+        return fibRecursive(n-1) + fibRecursive(n-2);
+    }
+
+    private static int fibMemoized(int n, int[] dp) {
+        if (n <= 1) return n;
+        if (dp[n] != -1) return dp[n];
+        dp[n] = fibMemoized(n-1, dp) + fibMemoized(n-2, dp);
+        return dp[n];
+    }
+
+    private static int fibTabulation(int n) {
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        // for(int i = 0; i <= n; i++) {
+        //     System.out.println(fib[i]);
+        // }
+        // System.out.println("The N'th Fibonacci is " + fib[n]);
+        return dp[n];
+    }
+}
